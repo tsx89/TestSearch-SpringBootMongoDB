ef29d17eb7b988f36f96a9d22c017d59473a87ff
TanSh1 <TanSh1@192.168.23.1>
2018-05-16 21:20:04 -0400

    remove

---
+++ MyTest2/src/main/java/com/example/MyTest/repository/InitialSearchAggregationOperations.java
@@ -0,0 +1,15 @@
+package com.example.MyTest.repository;
+
+import org.bson.Document;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperationContext;
+
+public class InitialSearchAggregationOperations implements AggregationOperation {
+
+	@Override
+	public Document toDocument(AggregationOperationContext arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
